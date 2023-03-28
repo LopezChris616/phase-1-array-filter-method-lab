d123b0dbb1f5ebd11dd5740f4cc6500a112afb49
Christian Lopez <lopezchristian1610@gmail.com>
2023-03-28 00:50:14 -0400

    all filter tests passed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution here
+function findMatching(arr, str) {
+    const matchedDrivers = arr.filter(driver => {
+        if(driver.toLowerCase() === str.toLowerCase()) {
+            return driver;
+        }
+    });
+    return matchedDrivers;
+}
+
+function fuzzyMatch(arr, str) {
+    const matchedDrivers = arr.filter(driver => {
+        let beginningLetters = "";
+        for (let i = 0; i <= str.length - 1; i++) {
+            beginningLetters += driver[i];
+        }
+        if (beginningLetters === str) {
+            return driver;
+        }
+    });
+    return matchedDrivers;
+}
+
+function matchName(arr, str) {
+    const matchedDrivers = arr.filter(driver => {
+        if(driver.name === str) {
+            return driver;
+        }
+    });
+    return matchedDrivers;
+}
